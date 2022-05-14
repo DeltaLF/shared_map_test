22a51dc8e68f0b2d35743fd45a53c8bd5544f357
DeltaLF <patricktsaikl@gmail.com>
2022-05-14 15:23:20 +0800

    UI to add, show marker with popup

--- public/javascripts/map/openStreetMap.js
+++ public/javascripts/map/openStreetMap.js
@@ -10,32 +10,68 @@ L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
     '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStretMap</a> contributors',
 }).addTo(map);
 
-var marker = L.marker([51.5, -0.09]).addTo(map);
+// utils
+function loc2str(loc) {
+  return parseFloat(loc).toFixed(2);
+}
 
-marker.bindPopup(" <h1>this is popup</h1> ").openPopup();
+// customized layer:
+const customizedLayer = L.layerGroup([]).addTo(map);
 
-var popup = L.popup()
-  .setLatLng([51.513, -0.09])
-  .setContent("I am a standalone popup.")
-  .openOn(map);
+var marker = L.marker([51.5, -0.09]).addTo(customizedLayer);
 
 function onMapClick(e) {
   console.log(e, e.latlng);
   const { lat, lng } = e.latlng;
   L.popup()
     .setLatLng([lat, lng])
-    .setContent(
-      `Clicked position: [${parseFloat(lat).toFixed(2)},${parseFloat(
-        lng
-      ).toFixed(2)}]`
-    )
+    .setContent(`Clicked position: [${loc2str(lat)}},${loc2str(lng)}]`)
     .openOn(map);
 }
 
 map.on("click", onMapClick);
 
 function onMapRightClick(e) {
-  alert(e);
+  // create new tag when right click
+  const { lat, lng } = e.latlng;
+  const input = prompt();
+  var marker = L.marker([lat, lng]).addTo(customizedLayer);
+  marker.bindPopup(input).openPopup();
 }
 
 map.on("contextmenu", onMapRightClick);
+
+function updatePanel() {
+  // clean up then append markers info to marker-list panel
+  const markerListDiv = $(".marker-list");
+  markerListDiv.children().remove();
+  const markerInLayer = customizedLayer.getLayers();
+  markerInLayer.forEach((marker) => {
+    console.log(marker);
+    const { lat, lng } = marker._latlng;
+    const popContent = marker.getPopup()
+      ? marker.getPopup()._content
+      : "No popup";
+
+    markerListDiv.append(
+      `<p>lat:${loc2str(lat)} lng:${loc2str(lng)} content: ${popContent}</p>`
+    );
+  });
+}
+
+$(".button-clear").on("click", function (e) {
+  console.log(e);
+  customizedLayer.clearLayers();
+});
+
+$(".button-show").on("click", function (e) {
+  // show layer in conosle
+  const layers = customizedLayer.getLayers();
+  console.log(layers);
+});
+
+$(".button-sync").on("click", function (e) {
+  // sync marker data in panel
+  // use onClick to update marker because there seems no onMarkerChange event
+  updatePanel();
+});
